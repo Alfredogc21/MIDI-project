46613fa69021bd6559e858631b341c54fdd30df3
Alfredo Gomez <alfretg.c@hotmail.com>
2024-04-22 04:03:17 -0500

    Creacion inicial del MIDI

---
+++ README.md
@@ -0,0 +1,17 @@
+## Proyecto de MIDI con HTML, CSS y JavaScript
+
+Este es un proyecto realizado de la mano de Alura Latam y Oracle, en el cual se desarrolla un MIDI, instrumento musical basico con interfaz digital.
+ 
+## Tecnologias utilizadas
+- HTML
+- CSS
+- JavaScript
+
+## Instrucciones de uso
+1. Clonar el repositorio
+2. Abrir el archivo `index.html` en un navegador web
+3. Seleccionar el instrumento deseado
+4. Seleccionar la opción deseada
+5. Disfrutar del sampler
+
+
---
+++ js/main.js
@@ -0,0 +1,26 @@
+function playSonido(idElementoAudio) {
+    document.querySelector(idElementoAudio).play(); // Reproducir sonido
+}
+
+// Nos traemos todos los elementos con la clase 'tecla'
+const listaDeTeclas = document.querySelectorAll('.tecla'); // Obtener lista de elementos con la clase 'tecla'
+
+let contador = 0;
+while (contador < listaDeTeclas.length) {
+
+    // Obtener elemento de la lista
+    const tecla = listaDeTeclas[contador];
+
+    // Obtener el nombre del instrumento
+    const instrumento = tecla.classList[1];
+
+    // Como el nombre del instrumento ya esta, solo necesitamos el #sonido_ + instrumento para que funcione el template string
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio);
+
+    // Asignar función al evento click
+    tecla.onclick = function () {
+        playSonido(idAudio);
+    }; 
+    contador++;
+}
